658e0f15c3893d9dfc03e2d728ac1d507a9d965e
vs <smoke.ltd.vs@gmail.com>
2024-04-12 11:31:25 +0300

    Define interface.

---
+++ lib/src/i_isar_preferences.dart
@@ -0,0 +1,31 @@
+abstract interface class IIsarPreferences {
+  Future<T> get<T>(String key);
+
+  Future<T> set<T>(String key, T value);
+
+  Future<T> update<T>(
+    String key,
+    T Function(T value) update, {
+    T Function()? ifAbsent,
+  });
+
+  Future<T> remove<T>(String key);
+
+  T getSync<T>(String key);
+
+  T setSync<T>(String key, T value);
+
+  T updateSync<T>(
+    String key,
+    T Function(T value) update, {
+    T Function()? ifAbsent,
+  });
+
+  T removeSync<T>(String key);
+
+  Future<T?> getOrNull<T>(String key);
+
+  T? getOrNullSync<T>(String key);
+
+  Stream<T?> watch<T>(String key);
+}
