b42230d6c7dd5e646444e35f697d910c3e49c054
vs <smoke.ltd.vs@gmail.com>
2024-04-16 22:17:43 +0300

    Add simple testing.

--- test/isar_preferences_test.dart
+++ test/isar_preferences_test.dart
@@ -1,12 +1,43 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:isar_preferences/isar_preferences.dart';
+import 'package:isar/isar.dart';
+import 'package:isar_preferences/src/isar_preferences.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  late IsarPreferences isarPreferences;
+
+  setUpAll(() async => await Isar.initializeIsarCore(download: true));
+
+  setUp(() async {
+    isarPreferences = await IsarPreferences.openIsarPreferences(
+      directory: '.',
+    );
+  });
+
+  tearDown(() {
+    isarPreferences.close(deleteFromDisk: true);
+  });
+
+  const nonExistingKey = 'nonExistingKey';
+  const key = 'key';
+  const key2 = 'key2';
+
+  test('value for non existing key is null', () async {
+    final result = await isarPreferences.getOrNull<String>(nonExistingKey);
+
+    expect(result, isNull);
+  });
+
+  test('set get remove async', () async {
+    const stringValue = 'value';
+
+    await isarPreferences.set<String>(key, stringValue);
+    final value = await isarPreferences.get<String>(key);
+
+    expect(value, stringValue);
+
+    await isarPreferences.remove<String>(key);
+
+    final newValue = await isarPreferences.getOrNull<String>(key);
+    expect(newValue, isNull);
   });
 }
